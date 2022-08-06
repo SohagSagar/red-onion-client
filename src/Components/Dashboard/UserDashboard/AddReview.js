94c1f76ecf55c796f00f4b12c7dadcd3fccf4b04
sohag sagar <sagorsohag29@gmail.com>
2022-08-06 18:08:52 +0600

    review form created

--- src/Components/Dashboard/UserDashboard/AddReview.js
+++ src/Components/Dashboard/UserDashboard/AddReview.js
@@ -1,9 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { MdError } from 'react-icons/md';
+import { BiCloudUpload } from 'react-icons/bi';
+import { TiTick } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+import { useQuery } from 'react-query';
 
 const AddReview = () => {
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+    const [loading, setLoading] = useState(false);
+    const [dataLoading, setDataLoading] = useState(false);
+
+
+    const requiredMessage = 'field is required';
+    // const capitalize = s => s && s[0].toUpperCase() + s.slice(1);
+    // const result=capitalize(requiredMessage);
+
+    //handle form data to sent to the database
+    const onSubmit = data => {
+        const addIteamData = {
+            ...data,
+
+        }
+        setDataLoading(true)
+        fetch('http://localhost:5000/add-food', {
+            method: 'POST',
+            headers: {
+                'content-type': "application/json"
+            },
+            body: JSON.stringify(addIteamData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                data?.insertedId ? toast.success('Iteam inserted successfully') : toast.error('Fail to insert iteam');
+                setDataLoading(false);
+                reset();
+
+
+            })
+
+
+
+
+    }
     return (
-        <div>
-            add review
+        <div class="rounded-md w-96 mx-auto bg-base-100 shadow-md mt-2">
+            <div class="card-body">
+                <h2 class="text-xl font-semibold text-center ">Add Review</h2><hr />
+
+                <form onSubmit={handleSubmit(onSubmit)} >
+
+                    {/* profession  name */}
+                    <label class="label  pb-0">
+                        <span class="label-text">Profession Name</span>
+                    </label>
+                    <input {...register('profession', {
+                        required: {
+                            value: true,
+                            message: requiredMessage
+                        },
+                        pattern: {
+                            value: /^[a-zA-Z\s]*$/g,
+                            message: 'Only text is allowed'
+                        }
+                    })} type="text" placeholder="Type here" class="input input-bordered input-sm w-full max-w-xs" />
+                    <label class="label py-0">
+                        {
+                            errors?.profession && <span class="label-text-alt text-rose-500"><MdError className='inline mb-1' />{errors.profession.message}</span>
+                        }
+                    </label>
+
+                    {/* ratings  */}
+                    <label class="label  pb-0">
+                        <span class="label-text">Ratings</span>
+                    </label>
+                    <select {...register('rating', {
+                        required: {
+                            value: true,
+                            message: 'No rating is selected'
+                        }
+                    })} class="select select-bordered select-sm w-full max-w-xs ">
+                        <option hidden></option>
+                        <option value={'1'}>1</option>
+                        <option value={'2'}>2</option>
+                        <option value={'3'}>3</option>
+                        <option value={'4'}>4</option>
+                        <option value={'5'}>5</option>
+                    </select>
+                    <label class="label pt-0">
+                        {
+                            errors.rating && <span class="label-text-alt text-rose-500"><MdError className='inline mb-1' />{errors.rating.message}</span>
+                        }
+
+                    </label>
+
+
+                    {/* your reviews  */}
+                    <div class="form-control">
+                        <label class="label pb-0">
+                            <span class="label-text">Review</span>
+                        </label>
+                        <textarea {...register('review', {
+                            required: {
+                                value: true,
+                                message: requiredMessage
+                            }
+                        })} class="textarea textarea-bordered h-24" placeholder="Write Your Review..."></textarea>
+                    </div>
+
+                    <label class="label pt-0">
+                        {
+                            errors?.review && <span class="label-text-alt text-rose-500"><MdError className='inline mb-1' />{errors.review.message}</span>
+                        }
+                    </label>
+
+
+                    <div class="card-actions justify-center  mt-5">
+                        <button type='submit' class={`btn btn-primary rounded-full w-full normal-case font-semibold ${dataLoading && 'loading'}`}>{!dataLoading && 'Submit Review'}</button>
+                    </div>
+
+                </form>
+
+
+            </div>
         </div>
     );
 };
